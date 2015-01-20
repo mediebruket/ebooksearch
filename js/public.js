52959931cbbe2ab192df923c1b7a75b5f9a4d085
Havard Grimelid <hgrimelid@gmail.com>
2015-01-20 10:53:04 +0100

    Reduce jQuery selector specificity in for more flexible markup.

--- js/public.js
+++ js/public.js
@@ -1,14 +1,14 @@
 // Tabbed content
 
 jQuery(document).ready(function(){
-    jQuery("ul#finneboktabs li").click(function(e){
+    jQuery("#finneboktabs li").click(function(e){
         if (!jQuery(this).hasClass("active")) {
             var tabNum = jQuery(this).index();
             var nthChild = tabNum+1;
-            jQuery("ul#finneboktabs li.active").removeClass("active");
+            jQuery("#finneboktabs li.active").removeClass("active");
             jQuery(this).addClass("active");
-            jQuery("ul#finneboktab li.active").removeClass("active");
-            jQuery("ul#finneboktab li:nth-child("+nthChild+")").addClass("active");
+            jQuery("#finneboktab li.active").removeClass("active");
+            jQuery("#finneboktab li:nth-child("+nthChild+")").addClass("active");
         }
     });
 });
@@ -25,13 +25,13 @@ function fbShare(url, winWidth, winHeight) {
 
 var TINY={};function T$(i){return document.getElementById(i)}function T$$(e,p){return p.getElementsByTagName(e)}TINY.accordion=function(){function slider(n){this.n=n;this.a=[]}slider.prototype.init=function(t,e,m,o,k){var a=T$(t),i=s=0,n=a.childNodes,l=n.length;this.s=k||0;this.m=m||0;for(i;i<l;i++){var v=n[i];if(v.nodeType!=3){this.a[s]={};this.a[s].h=h=T$$(e,v)[0];this.a[s].c=c=T$$('div',v)[0];h.onclick=new Function(this.n+'.pr(0,'+s+')');if(o==s){h.className=this.s;c.style.height='auto';c.d=1}else{c.style.height=0;c.d=-1}s++}}this.l=s};slider.prototype.pr=function(f,d){for(var i=0;i<this.l;i++){var h=this.a[i].h,c=this.a[i].c,k=c.style.height;k=k=='auto'?1:parseInt(k);clearInterval(c.t);if((k!=1&&c.d==-1)&&(f==1||i==d)){c.style.height='';c.m=c.offsetHeight;c.style.height=k+'px';c.d=1;h.className=this.s;su(c,1)}else if(k>0&&(f==-1||this.m||i==d)){c.d=-1;h.className='';su(c,-1)}}};function su(c){c.t=setInterval(function(){sl(c)},20)};function sl(c){var h=c.offsetHeight,d=c.d==1?c.m-h:h;c.style.height=h+(Math.ceil(d/5)*c.d)+'px';c.style.opacity=h/c.m;c.style.filter='alpha(opacity='+h*100/c.m+')';if((c.d==1&&h>=c.m)||(c.d!=1&&h==1)){if(c.d==1){c.style.height='auto'}clearInterval(c.t)}};return{slider:slider}}();
 
-// hide missing images 
+// hide missing images
 jQuery("img").error(function(){
         $(this).hide();
 });
 
 // Start Ready
-jQuery(document).ready(function() {  
+jQuery(document).ready(function() {
 
 
 	// Live Search
@@ -40,7 +40,7 @@ jQuery(document).ready(function() {
 	    var query_value = jQuery('input#search').val();
 		var makstreff = jQuery('input#finnebok_makstreff').val();
 		var formater = jQuery('input#pdf:checked').val() + jQuery('input#epub:checked').val();
-	    jQuery('b#finnebok_search-string').html(query_value);
+	    jQuery('#finnebok_search-string').html(query_value);
 		if(query_value !== '') {
 			jQuery.ajax({
 				type: "POST",
@@ -51,7 +51,7 @@ jQuery(document).ready(function() {
 					jQuery("#finnebok_results").html(html);
 				}
 			});
-		}return false;    
+		}return false;
 	}
 
 	//jQuery("input#search").live("keyup", function(e) {
@@ -66,13 +66,13 @@ jQuery(document).ready(function() {
 	    //if (search_string == '') {
 		if (search_string.length < 3) {
 		   	jQuery("#finnebok_results").fadeOut();
-	    	jQuery('h4#results-text').fadeOut();
+	    	jQuery('#results-text').fadeOut();
 	    }else{
 	    	jQuery("#finnebok_results").fadeIn();
-	    	jQuery('h4#results-text').fadeIn();
+	    	jQuery('#results-text').fadeIn();
 	    	jQuery(this).data('timer', setTimeout(search, 100));
 	    };
-	    
+
 	});
 
 });
